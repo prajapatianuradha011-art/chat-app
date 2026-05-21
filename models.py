62c071916f2d2c37c01221e44f7e2cd850cb5a05
Anurag <prajapatianuradha011@gmail.com>
2026-05-21 18:58:33 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class Message(Base):
-    __tablename__ = "messages"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    text = Column(String)
